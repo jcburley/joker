429fcd97cf21ff6e761a106236c91249bb340243
James Craig Burley <james-github@burleyarch.com>
2018-12-17 16:02:47 -0500

    Subdir was renamed

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e  # Exit on error.
 
 if [ -e GO.link ]; then
-    go run tools/gostd/main.go --replace --joker .
+    go run tools/_gostd/main.go --replace --joker .
 fi
 
 go generate ./... && go tool vet -all -shadow=true main.go && go tool vet -all -shadow=true core std && go build && ./joker "$@"
