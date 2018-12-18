f59b2a1546cb3810fee2620405c92a70f81a4de6
James Craig Burley <james-github@burleyarch.com>
2018-12-17 19:08:28 -0500

    Merge smarter-run-sh run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 
+build() {
+    go clean -x
+
+    go generate ./...
+
+    go tool vet -all -shadow=true ./
+
+    go build
+}
+
 set -e  # Exit on error.
 
-if [ -e GO.link ]; then
-    go run tools/_gostd/main.go --replace --joker .
+build
+
+./joker -e '(print "\nLibraries available in this build:\n  ") *loaded-libs* (println)'
+
+SUM256="$(go run tools/sum256dir/main.go std)"
+
+(cd std; ../joker generate-std.joke)
+
+NEW_SUM256="$(go run tools/sum256dir/main.go std)"
+
+if [ "$SUM256" != "$NEW_SUM256" ]; then
+    cat <<EOF
+Rebuilding Joker, as the libraries have changed; then regenerating docs.
+
+EOF
+    build
+
+    (cd docs; ../joker generate-docs.joke)
 fi
 
-go generate ./... && go tool vet -all -shadow=true main.go && go tool vet -all -shadow=true core std && go build && ./joker "$@"
+./joker "$@"
