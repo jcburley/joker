d15e16ba43d11e870b87d3c72d9fa2f10b40f2b0
James Craig Burley <james-github@burleyarch.com>
2018-12-18 16:15:30 -0500

    Regenerate

--- tools/_gostd/tests/gold/amd64-linux/joker/custom.go
+++ tools/_gostd/tests/gold/amd64-linux/joker/custom.go
@@ -1,3 +1,12 @@
-// Placeholder for custom libraries. Overwritten by gostd.
+// Auto-modified by gostd at (omitted for testing)
 
 package main
+
+import (
+	_ "github.com/candid82/joker/std/go/std/net"
+	_ "github.com/candid82/joker/std/go/std/net/http"
+	_ "github.com/candid82/joker/std/go/std/net/mail"
+	_ "github.com/candid82/joker/std/go/std/net/rpc"
+	_ "github.com/candid82/joker/std/go/std/net/textproto"
+	_ "github.com/candid82/joker/std/go/std/net/url"
+)
