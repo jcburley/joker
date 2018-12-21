d799af559acc3924fcf4933dc0a39f268ea63687
James Craig Burley <james-github@burleyarch.com>
2018-12-21 12:19:54 -0500

    Get more array stuff working for input args
    This now works, and is pretty cool:
    
    ```
    user=> (require '[go.std.bytes :as b])
    nil
    user=> (apply str (map char (b/Title (mapv int (vec "hey there")))))
    "Hey There"
    user=>
    ```

--- core/custom-runtime.go
+++ core/custom-runtime.go
@@ -1,13 +1,31 @@
 package core
 
+import (
+	"fmt"
+)
+
 func ConvertToArrayOfByte(o Object) []byte {
 	switch obj := o.(type) {
 	case String:
 		return []byte(obj.S)
 	case *Vector:
-		return []byte("TODO(BYTE)")
+		by := make([]byte, obj.Count())
+		for i := 0; i < obj.Count(); i++ {
+			el := obj.Nth(i)
+			if val, ok := el.(Int); ok {
+				b := val.I
+				if b >= 0 && b <= 255 {
+					by[i] = byte(b)
+				} else {
+					panic(RT.NewError(fmt.Sprintf("Element %d out of range (%d) for Byte: %s", i, b, obj.ToString(false))))
+				}
+			} else {
+				panic(RT.NewError(fmt.Sprintf("Element %d not convertible to Byte: %s", i, el.ToString(true))))
+			}
+		}
+		return by
 	default:
-		return []byte("FAIL(BYTE)")
+		panic(RT.NewError(fmt.Sprintf("Not convertible to array of Byte: %s", obj.ToString(true))))
 	}
 }
 
@@ -21,7 +39,11 @@ func ConvertToArrayOfInt(o Object) []int {
 }
 
 func ConvertToArrayOfString(o Object) []string {
-	return []string{"FAIL", "ConvertToArrayOfString", "TOO"}
+	switch o.(type) {
+	case *Vector:
+	default:
+	}
+	return []string{"NOT", "YET"}
 }
 
 func ConvertToArrayOfObject(o Object) []Object {
