69cd44de7d865a1667c53c47d663b2bb113cd0d8
James Craig Burley <james-github@burleyarch.com>
2019-01-07 11:06:29 -0500

    Update amd64-linux tests

--- tools/gostd/_tests/gold/amd64-linux/joker/custom.go
+++ tools/gostd/_tests/gold/amd64-linux/joker/custom.go
@@ -5,10 +5,6 @@ package main
 import (
 	_ "github.com/candid82/joker/std/go/std/net"
 	_ "github.com/candid82/joker/std/go/std/net/http"
-	_ "github.com/candid82/joker/std/go/std/net/mail"
-	_ "github.com/candid82/joker/std/go/std/net/rpc"
-	_ "github.com/candid82/joker/std/go/std/net/rpc/jsonrpc"
-	_ "github.com/candid82/joker/std/go/std/net/smtp"
 	_ "github.com/candid82/joker/std/go/std/net/textproto"
 	_ "github.com/candid82/joker/std/go/std/net/url"
 )
