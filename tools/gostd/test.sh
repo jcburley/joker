a7633eb92be433163d1d1a954ab71660a51254dd
James Craig Burley <james-github@burleyarch.com>
2019-01-13 20:08:28 -0500

    Eliminate $GOSRC entirely; support --reset option

--- tools/gostd/test.sh
+++ tools/gostd/test.sh
@@ -6,6 +6,7 @@ mkdir -p "$GOENV"
 EXIT="exit 99"
 if [ "$1" = "--on-error" ]; then
     EXIT="$2"
+    shift 2
 fi
 
 RC=0
@@ -16,6 +17,10 @@ cp -pv ../../custom.go $GOENV/joker/
 cp -pv ../../core/data/core.joke $GOENV/joker/core/data/
 cp -pv ../../std/generate-custom.joke $GOENV/joker/std/
 
+if [ "$1" = "--reset" ]; then
+    exit 0
+fi
+
 [ ! -x gostd ] && echo >&2 "No executable to test." && exit 99
 
 ./gostd --no-timestamp --output-code --verbose --go _tests/small 2>&1 | grep -v '^Default context:' > $GOENV/small.gold
@@ -24,7 +29,7 @@ git diff --quiet -u $GOENV/small.gold || { echo >&2 "FAILED: small test"; RC=1; 
 ./gostd --no-timestamp --output-code --verbose --go _tests/big --replace --joker $GOENV/joker 2>&1 | grep -v '^Default context:' > $GOENV/big.gold
 git diff --quiet -u $GOENV/big.gold || { echo >&2 "FAILED: big test"; RC=1; $EXIT; }
 
-./gostd --no-timestamp --output-code --verbose --go "$GOSRC" 2>&1 | grep -v '^Default context:' > $GOENV/gosrc.gold
-git diff --quiet -u $GOENV/gosrc.gold || { echo >&2 "FAILED: \$GOSRC test"; RC=1; $EXIT; }
+./gostd --no-timestamp --output-code --verbose 2>&1 | grep -v '^Default context:' > $GOENV/gosrc.gold
+git diff --quiet -u $GOENV/gosrc.gold || { echo >&2 "FAILED: gosrc test"; RC=1; $EXIT; }
 
 exit $RC
