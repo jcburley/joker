b36e61a8a132f969b59b2e305c9b94b6eed8fcee
James Craig Burley <430319+jcburley@users.noreply.github.com>
2019-10-20 18:57:28 -0400

    Have to delete docs/index.html after re-running Joker to clean up

--- clean.sh
+++ clean.sh
@@ -2,8 +2,11 @@
 
 # Undo "side-effects" of running tools/gostd/gostd.
 
-rm -fr docs/index.html docs/go.std.* core/a_*_data.go
+rm -fr docs/go.std.* core/a_*_data.go
 
 (cd tools/gostd && go build .) && ./tools/gostd/gostd --undo --joker .
 
 (cd docs; ../joker generate-docs.joke --no-go)
+
+# Delete regenerated file that is not in the repo (in this fork/branch).
+rm -fr docs/index.html
