845b7513b3743f472f95197b00cefce633fa382f
James Craig Burley <430319+jcburley@users.noreply.github.com>
2020-07-28 00:10:00 -0400

    [WiP] Get "string" type working
    This restores net/url.PathEscape as a generated function, among a few others.

--- tools/gostd/types.go
+++ tools/gostd/types.go
@@ -7,6 +7,18 @@ import (
 	//	"strings"
 )
 
+var typeMap = map[string]jtypes.Info{}
+
 func JokerTypeInfoForExpr(e Expr) jtypes.Info {
+	switch td := e.(type) {
+	case *Ident:
+		if jti, found := typeMap[td.Name]; found {
+			return jti
+		}
+	}
 	return jtypes.Nil
+}
+
+func init() {
+	typeMap["string"] = jtypes.String
 }
