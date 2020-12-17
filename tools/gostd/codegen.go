7ce4d84c456ed53db8babb68fff4f9682b53235a
James Craig Burley <430319+jcburley@users.noreply.github.com>
2020-12-17 15:06:10 -0500

    Get Joker vetting and building against go1.16beta1
    Doesn't pass tests because of the type-alias issue, which can be fixed at the test level (if not by actually supporting them in Joker).
    
    But this addresses the failure to vet as a result of:
    
    https://github.com/golang/go/issues/40701

--- tools/gostd/codegen.go
+++ tools/gostd/codegen.go
@@ -443,21 +443,27 @@ func ExtractGoObject%s(args []Object, index int) *%s {
 	switch o := a.(type) {
 	case GoObject:
 		switch r := o.O.(type) {
-		case %s:
-			return &r
-		case *%s:
+%s		case *%s:
 			return r
 		}
 	%s}
 	panic(RT.NewArgTypeError(index, a, "GoObject[%s]"))
 }
 `
+	const goExtractRefToTemplate = `
+		case %s:
+			return &r
+`
 
 	baseTypeName := ts.Name.Name
 	typeName := myGoImport + "." + baseTypeName
 
 	others := maybeImplicitConvert(godb.GoFileForTypeSpec(ts), typeName, ts)
-	goc := fmt.Sprintf(goExtractTemplate, baseTypeName, typeName, typeName, typeName, others, t)
+	goExtractRefTo := ""
+	if !(ti.GoPackage() == "reflect" && (ti.GoBaseName() == "StringHeader" || ti.GoBaseName() == "SliceHeader")) {
+		goExtractRefTo = fmt.Sprintf(goExtractRefToTemplate[1:], typeName)
+	}
+	goc := fmt.Sprintf(goExtractTemplate, baseTypeName, typeName, goExtractRefTo, typeName, others, t)
 
 	goc += goTypeExtractor(t, ti)
 
@@ -472,6 +478,9 @@ var CtorNames = map[TypeInfo]string{}
 
 func genCtor(tyi TypeInfo) {
 	if !tyi.Custom() {
+		return
+	}
+	if tyi.GoPackage() == "reflect" && (tyi.GoBaseName() == "StringHeader" || tyi.GoBaseName() == "SliceHeader") {
 		return
 	}
 
