ff35b43c9c5fa21f5c90f5276d56824ae83c5b39
James Craig Burley <430319+jcburley@users.noreply.github.com>
2020-12-24 12:18:19 -0500

    [WiP] Add type assertions to .(Native) assertions for func args

--- tools/gostd/pre.go
+++ tools/gostd/pre.go
@@ -17,7 +17,7 @@ func genTypePre(fn *FuncInfo, indent string, e Expr, paramName string, argNum in
 
 	clType, clTypeDoc, goType, goTypeDoc = ti.ClojureName(), ti.ClojureNameDoc(e), goName, ti.GoNameDoc(e)
 	if fn.Fd == nil || fn.Fd.Recv != nil {
-		goPreCode = fmt.Sprintf("%s := SeqNth(_argList, %d).(Native)", paramName, argNum)
+		goPreCode = fmt.Sprintf("%s := SeqNth(_argList, %d).(Native).(%s)", paramName, argNum, goName)
 	}
 	if ti.IsPassedByAddress() {
 		cl2golParam = "*" + paramName
