d3fd93b4449e15c72fb1e134465bc4a6402390c3
James Craig Burley <430319+jcburley@users.noreply.github.com>
2021-01-04 19:43:53 -0500

    Instead of running gostd --undo, delete generated stuff and restore template files via script

--- clean.sh
+++ clean.sh
@@ -6,10 +6,14 @@ set -e  # Exit on error.
 
 rm -fr docs/go.std.* core/a_*_data.go
 
-(cd tools/gostd && go build .)
-./tools/gostd/gostd --undo --clojure .
+# Restore original versions of generated files so vanilla Joker can build.
+rm -f g_* core/g_* core/data/g_*
+rm -fr std/go
+cp core/go_templates/g_goswitch.gotemplate core/g_goswitch.go
+cp core/go_templates/g_customlibs.joketemplate core/data/g_customlibs.joke
 
-(cd docs; ../joker generate-docs.joke --no-go || ../joker-good generate-docs.joke --no-go || :)  # ok if failure here
+# Ok if failure here.
+(cd docs; ../joker generate-docs.joke --no-go || ../joker-good generate-docs.joke --no-go || :)
 
 # Delete regenerated file that is not in the repo (in this fork/branch).
 rm -fr docs/index.html
